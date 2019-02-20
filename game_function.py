cd780893687f8da55e26bf8677e5a5c1aacb4403
Juliane Regina Venteu <juliane.venteu@sp01.local>
2019-02-20 13:25:37 -0300

    Refactoring function check_events

--- game_function.py
+++ game_function.py
@@ -6,23 +6,28 @@ def check_events(ship):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-        
-        #check if the key was pressed
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
 
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
+        if event.type == pygame.KEYDOWN:
+            check_keydown_events(event,ship)
 
-        #check if the key was release
-        elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:                
-                ship.moving_right = False
+        if event.type == pygame.KEYUP:     
+            check_keyup_events(event,ship)             
 
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
+def check_keyup_events(event,ship):
+    #check if the key was release
+    if event.key == pygame.K_RIGHT:                
+        ship.moving_right = False
 
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
+def check_keydown_events(event,ship):
+    #check if the key was pressed
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
 
 def udpdate_screen(config, screen, ship):
     """ Update the images on screen and show the new screen"""
